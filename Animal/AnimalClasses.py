e7f02dea29e81075d7a52ff01e1389e66955aa11
DugaCeline <81234249+DugaCeline@users.noreply.github.com>
2024-04-17 12:12:10 +0300

    Update AnimalClasses.py

--- Animal/AnimalClasses.py
+++ Animal/AnimalClasses.py
@@ -4,12 +4,13 @@
 Define a class hierarchy representing animals with a parent class Animal
 and child classes such as dog, fish and bird. Each subclass animal should have
 a name and an age and should have 2 methods defined in it: move(), eat().
-The move method needs to be specific for a given animal where as the
-eat() method should be generic for all animals. The methods don't need to
-do anything except print information about who is doing what.
+The methods should be specific for the animal, and the general animal should not be able to use 
+move() or eat() method. (Study abstraction)
+The methods don't need to do anything except print information about who is doing what.
 
 After creating the class hierarchy, create instances of each of the 3 animals
 dog, fish and bird and make them eat and move.
+
 
 """
 # Your Code Below:
@@ -21,16 +22,31 @@ class Animal:
         self.age = Age
 
     def move(self):
-        print(self.name + " is moving")
+        raise NotImplementedError("Child class should be implementing move method")
 
     def eat(self):
-        print(self.name + " is eating")
+        raise NotImplementedError("Child class should be implementing eat method")
 
 class Dog(Animal):
-    pass
+
+    def eat(self):
+        print(self.name + " is eating bones")
+
+    def move(self):
+        print(self.name + " is running")
 
 class Fish(Animal):
-    pass
+
+    def eat(self):
+        print(self.name + " is eating seaweed")
+
+    def move(self):
+        print(self.name + " is swimming")
 
 class Bird(Animal):
-    pass
+
+    def eat(self):
+        print(self.name + " is eating seeds")
+
+    def move(self):
+        print(self.name + " is flying")
