4daa9e2c9f76e4f497c7aef4b686b9980efb609e
DugaCeline <81234249+DugaCeline@users.noreply.github.com>
2024-04-17 12:12:36 +0300

    Update Assignment.py

--- Animal/Assignment.py
+++ Animal/Assignment.py
@@ -1,15 +1,21 @@
 from Animal.AnimalClasses import *
 
+animal = Animal("King", 22)
 dog = Dog("Max",5)
 bird = Bird("Tweety",2)
 fish = Fish("Golden",1)
 
 
 print(dog.eat())
-print(bird.eat())
-print(fish.eat())
-
-
 print(dog.move())
+
+print(bird.eat())
 print(bird.move())
+
+print(fish.eat())
 print(fish.move())
+
+print(animal.eat())
+print(animal.move())
+
+
